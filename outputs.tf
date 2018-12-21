5b1bdcc46ceb84a044f7c4b983b61439aa166dba
Mathieu Debove <debovemathieu@gmail.com>
2018-12-21 16:44:35 +0100

    Add outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output "alb_dns_name" {
   value = "${module.alb.alb_dns_name}"
 }
+
+output "vpc_id" {
+  value = "${module.vpc.vpc_id}"
+}
+
+output "alb_target_group_arn" {
+  value = "${module.alb.default_target_group_arn}"
+}
+
+output "private_subnet_ids" {
+  value = ["${module.subnets.private_subnet_ids}"]
+}
